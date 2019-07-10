a2a8f826ce82c70c8c2b62a7d405d02930e2043f
b1900016 <b1900016@users.noreply.github.com>
2019-07-11 00:52:35 +0800

    BIT102 Website

---
+++ responsive.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+const navSlide = () => {
+    const opener = document.querySelector('.opener');
+    const nav = document.querySelector('.nav-link');
+
+    opener.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        opener.classList.toggle('ani')
+    });
+}
+navSlide();
+
